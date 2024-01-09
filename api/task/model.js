5e99fb7764e8225940760db8ad4b41f1101f0095
David Tacoronte <d.tacoronte89@gmail.com>
2024-01-08 21:48:16 -0500

    wip

--- api/task/model.js
+++ api/task/model.js
@@ -6,6 +6,35 @@ async function get() {
         .select(
             't.task_id',
             't.task_description',
-            t.task_notes
+            't.task_notes',
+            'p.project_name',
+            'p.project_description'
         )
+
+        const result = rows.map((row) => {
+            return {
+                ...row,
+                task_completed: row.task_completed ? true : false
+            }
+        })
+        return result
+}
+
+async function create(task) {
+    const [newId] = await db('tasks').insert(task)
+
+    const newTask = await db('tasks').where('task_id', newId)
+
+    const result = newTask.map((row) => {
+        return {
+            ...row,
+            task_completed: row.task_completed ? true : false
+        }
+    })
+    return result[0]
+}
+
+module.exports = {
+    get,
+    create,
 }
